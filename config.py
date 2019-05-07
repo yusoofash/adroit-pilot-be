4de818c816901c1d04a83ebfb8744bb7a151528f
yusoofash <41569614+yusoofash@users.noreply.github.com>
2019-05-07 13:00:21 +0530

    git ignore

---
+++ config.py
@@ -0,0 +1,7 @@
+MONGO_URI = 'mongodb://clark_kent:adroitpilot1997@ds135413.mlab.com:35413/adroit_pilot'
+JWT_SECRET_KEY = 'adroitu9z0_lff3i--qp7lyty8r3kl^m&hnciq)fadpv!8)_(6qp7^x&pilot'
+CLOUDINARY_CLOUD_NAME = 'drvpgz7gl'
+CLOUDINARY_API_KEY = '162242658189195'
+CLOUDINARY_API_SECRET = 'kyYp_dzZqdLSe-by_7rXzPMZ8Lc'
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx'])
